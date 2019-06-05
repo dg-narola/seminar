06e832008d9575384bdd15eb73e97876aa31568f
dg-narola <dg@narola.email>
2019-06-05 15:26:17 +0530

    changes

--- spec/models/seminartopic_spec.rb
+++ spec/models/seminartopic_spec.rb
@@ -12,5 +12,4 @@ RSpec.describe Seminartopic, type: :model do
     it { is_expected.to validate_presence_of(:title)}
     it { is_expected.to validate_presence_of(:descripton)}
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,4 @@ RSpec.describe User, type: :model do
     # it { is_expected.to validate_confirmation_of(:email) }
     it { is_expected.to validate_presence_of(:password).on(:create) }
   end
-
 end
